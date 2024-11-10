d35779f1a23a7497818df5009f44a076610a0baa
hduy0407 <t3ngdu01@students.oamk.fi>
2024-11-10 18:57:49 +0200

    complete

--- server/routers/todoRouter.js
+++ server/routers/todoRouter.js
@@ -1,6 +1,5 @@
-import { pool } from "../helpers/db.js";
+
 import { Router } from "express";
-import { auth } from "../helpers/auth.js";
 import { getTasks, postTask, deleteTask } from "../controllers/TaskController.js";
 
 const router = Router()
